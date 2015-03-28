c56f093c59a78cbd8dd0cfddc46bc8ec35f1993f
Itachi0970 <dkennygold@hotmail.com>
2015-03-28 13:09:05 -0600

    random numbers

--- logic.s
+++ logic.s
@@ -1,7 +1,7 @@
 oneTurn:
 	//detect inputs subroutine
 	//put start button value memory location in r2
-	mov r2, [r2]
+	ldr r2, [r2]
 	mov r1, #0
 	cmp r1, r2
 	beq //pause menu subroutine
@@ -9,10 +9,10 @@ oneTurn:
 	//put down button value memory location in r3
 	//put left button value memory location in r4
 	//put right button value memory location in r5
-	mov r2, [r2]
-	mov r3, [r3]
-	mov r4, [r4]
-	mov r5, [r5]
+	ldr r2, [r2]
+	ldr r3, [r3]
+	ldr r4, [r4]
+	ldr r5, [r5]
 	cmp r1, r2
 	beq //move up subroutine
 	cmp r1, r3
@@ -21,18 +21,28 @@ oneTurn:
 	beq //move left subroutine
 	cmp r1, r5
 	beq //move right subroutine
-	//put a button value memory location in r2
-	mov r2, [r2]
+	//put 'a' button value memory location in r2
+	ldr r2, [r2]
 	cmp r1, r2
 	beq //shoot bullet subroutine
 	
 	mov r1, #0
 	mov r0, #17
 npcStuff:
+	mov r4, #0
 	add r1, r1, #1
 	cmp r1, r0
 	bge afterNpc
-	//put random number %4 in r2
+	//make r4 = 1 if current npc is dead
+	cmp r1, #1
+	bge npcStuff
+	//puts random number %4 in r2
+	mov r3, #69069
+	ldr r2, =x20003004
+	ldr r2, [r2] //current clock value
+	mul r2, r3, r2
+	add r2, r3, r2
+	mov r2, r2 MOD 4 //this might not work
 	cmp r2, #0
 	beq //move npc up subroutine
 	cmp r2, #1
@@ -41,7 +51,12 @@ npcStuff:
 	beq //move npc left subroutine
 	cmp r2, #3
 	beq //move npc right subroutine
-	//put random number %10 in r2
+	//puts random number %10 in r2
+	ldr r2, =x20003004
+	ldr r2, [r2] //current clock value
+	mul r2, r3, r2
+	add r2, r3, r2
+	mov r2, r2 MOD 10 //this might not work
 	cmp r2, #0
 	beq //shoot npc bullet subroutine
 	b npcStuff
