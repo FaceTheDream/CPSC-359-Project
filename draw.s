21d05b67fa7358d815435a5f7818f8be6e10c442
Kyle Buettner <kindlylaughter@gmail.com>
2015-03-29 07:12:37 -0600

    Update drawPixel subroutine
    Changed 8-bit colour drawPixel to 32-bit colour drawPixel

--- draw.s
+++ draw.s
@@ -41,7 +41,7 @@ drawPixel: //r0 is assumed to be the x location, r1 is assumed to be the y locat
 	
 	mul    r1,    #1024                  //row-major
 	add    r0,    r1                     //
-	lsl    r0,    #3                     //8-bit colour assumed
+	lsl    r0,    #5                     //32-bit colour assumed
 	ldr    r1,    =frameBufferPointer    // should get frameBuffer location from file that contains frameBuffer information
 	ldr    r1,    [r1]                   //
 	add    r1,    r0                     // add offset
