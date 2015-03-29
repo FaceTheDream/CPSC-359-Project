47e77d036c1ed197ccf67a0ad7719744a5e924a2
Itachi0970 <dkennygold@hotmail.com>
2015-03-28 18:10:35 -0600

    Update logic.s

--- logic.s
+++ logic.s
@@ -34,6 +34,26 @@ start:
 	ldr r0, =crntpause
 	mov r1, #0
 	str r1, [r0]
+	ldr r0, =obstaclexs
+	mov r1, #128
+	str r1, [r0]
+	mov r1, #256
+	str r1, [r0, #4]
+	mov r1, #384
+	str r1, [r0, #8]
+	mov r1, #512
+	str r1, [r0, #12]
+	mov r1, #640
+	str r1, [r0, #16]
+	ldr r0, =obstacleys
+	mov r1, #256
+	str r1, [r0, #8]
+	mov r1, #512
+	str r1, [r0]
+	str r1, [r0, #16]
+	mov r1, #768
+	str r1, [r0, #4]
+	str r1, [r0, #12]
 	ldr r0, =npcys
 	mov r1, #0
 	mov r2, #0
@@ -126,17 +146,147 @@ npcStuff:
 afterNpc:
 	//detect collisions:
 	//if bullet is overlapped by player, player is hit
-		//make a loop which compares location of each bullet's front pixel to each location of player's drawn shape
+	mov r4, #0	//bullet#
+	mov r5, #14	//max bullets
+	
+colLoop:
+	cmp r4, r5
+	beq colLoop2
+	ldr r0, =playerx
+	ldr r0, [r0]
+	ldr r1, =playery
+	ldr r1, [r1]
+	ldr r2, =bulletxs
+	ldr r2, [r2, r4, lsl #2]
+	ldr r3, =bulletys
+	ldr r3, [r3, r4, lsl #2]
+	cmp r0, r2
+	beq colLoopY
+	add r4, r4, #1
+	b colLoop
+	
+colLoopY:
+	cmp r1, r3
+	beq scoreDown
+	add r4, r4, #1
+	b colLoop
+	
+scoreDown:
+	ldr r6, =score
+	ldr r6, [r6]
+	sub r6, r6, #10
+	cmp r6, #0
+	ble gameOver
+	add r4, r4, #1
+	b colLoop
+	
+colLoop2:
+	mov r4, #0	//bullet#
+	mov r5, #0	//npc#
+	mov r6, #14	//max bullets
+	mov r7, #16	//max npcs
+	
+colLoop2x:
+	cmp r4, r6
+	beq colLoopNpc
 	//if bullet is overlapped by npc, npc is hit
+	ldr r0, =bulletxs
+	ldr r0, [r0, r4, lsl #2]
+	ldr r1, =npcxs
+	ldr r1, [r1, r5, lsl #2]
+	cmp r0, r1
+	beq colLoop2y
+	add r4, r4, #1
+	b colLoop2x
+	
+colLoop2y:
+	ldr r0, =bulletys
+	ldr r0, [r0, r4, lsl #2]
+	ldr r1, =npcys
+	ldr r1, [r1, r5, lsl #2]
+	cmp r0, r1
+	beq npcHit
+	add r4, r4, #1
+	b colLoop2x
+
+colLoopNpc:
+	cmp r5, r7
+	beq colLoop3
+	mov r4, #0
+	add r5, r5, #1
+	b colLoop2x
+	
+npcHit:
+	// r5 = npc#
+	ldr r8, =npchp
+	ldr r9, [r8, r5, lsl #2]
+	cmp r9, #1
+	beq npcDie
+	b npcHit2
+	
+npcDie:
+	ldr r11, =score
+	ldr r10, [r11]
+	add r10, r10, #5
+	cmp r5, #10
+	addge r10, r10, #5
+	cmp r5, #15
+	addge r10, r10, #90
+	str r10, [r11]
+	
+npcHit2:
+	sub r9, r9, #1
+	str r9, [r8, r5, lsl #2]
+	add r4, r4, #1
+	b colLoop2x
+	
+colLoop3:
+	mov r4, #0	//bullet#
+	mov r5, #0	//obstacle#
+	mov r6, #14	//max bullets
+	mov r7, #4	//max obstacles
+	
+colLoop3x:
+	cmp r4, r6
+	beq colLoopObs
 	//if bullet is overlapped by obstacle, obstacle is hit
-	//if player is hit, score - 10
-	//if npc is hit, hp - 1
-	//if npc hp = 0, kill npc and increase score
-	//if npc is hit and hp != 0, shrink npc
-	//if obstacle is hit, hp - 1
-	//if obstacle hp = 0, kill obstacle
-	//if obstacle is hit and hp != 0, shrink obstacle
-	//draw screen subroutine
+	ldr r0, =bulletxs
+	ldr r0, [r0, r4, lsl #2]
+	ldr r1, =obstaclexs
+	ldr r1, [r1, r5, lsl #2]
+	cmp r0, r1
+	beq colLoop3y
+	add r4, r4, #1
+	b colLoop3x
+	
+colLoop3y:
+	ldr r0, =bulletys
+	ldr r0, [r0, r4, lsl #2]
+	ldr r1, =obstacleys
+	ldr r1, [r1, r5, lsl #2]
+	cmp r0, r1
+	beq obsHit
+	add r4, r4, #1
+	b colLoop3x
+
+colLoopObs:
+	cmp r5, r7
+	beq colEnd
+	mov r4, #0
+	add r5, r5, #1
+	b colLoop3x
+	
+obsHit:
+	// r5 = obstacle#
+	ldr r8, =obstaclehp
+	ldr r9, [r8, r5, lsl #2]
+	sub r9, r9, #1
+	str r9, [r8, r5, lsl #2]
+	add r4, r4, #1
+	b colLoop3x
+	
+colEnd:
+	//DRAW SCREEN NOW
 	b oneTurn
 	
 pauseMenu:
@@ -145,7 +295,7 @@ pauseMenu:
 	ldr r2, [r2]
 	mov r1, #0
 	cmp r1, r2
-	b endSub
+	beq endSub
 	//put up button value memory location in r2
 	//put down button value memory location in r3
 	ldr r2, [r2]
@@ -267,6 +417,15 @@ npcRight:
 	str r2, [r1, r0, lsl #2]
 	bx lr
 
+gameOver:
+	//detect inputs
+	//put start button value memory location in r2
+	ldr r2, [r2]
+	mov r1, #0
+	cmp r1, r2
+	beq start
+	b gameOver
+	
 endSub:
 	bx lr
 	
@@ -281,3 +440,8 @@ npcxs:		.int	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 npcys:		.int	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 npchp:		.int	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3
 crntpause:	.int	0 //0 = resume game, 1 = restart game, 2 = quit game
+bulletxs:	.int	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+bulletys:	.int	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+obstaclexs:	.int	0, 0, 0, 0, 0
+obstacleys:	.int	0, 0, 0, 0, 0
+obstaclehp:	.int	0, 0, 0, 0, 0
