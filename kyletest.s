6d702935b1700bf40dd43a68a7e2b5b0700f4d83
juchu <juchu@pi-4a.cs.ucalgary.ca>
2015-03-30 00:14:27 -0600

    asdf

--- kyletest.s
+++ kyletest.s
@@ -10,50 +10,63 @@ starTest:
 	bl drawGameTitle
 	pop {r0}
 	
-testPawn:
+/*testPawn:		
+//currently draws a yellow rectangle, a small vertical black line, and a long horizontal yellow line
+//will not draw anything after drawn
 	ldr r0, =0x180			//x coordinate of pawn bee
 	ldr r1, =0xa			//y coordinate of pawn bee
 	push {r0, r1}
 	bl drawBeeP
 	pop {r0, r1}
+
 	
-testKnight:
+testKnight:		
+//currently draws a yellow rectangle to the right and down of where it should be, a thicker black vertical line, and a long horizontal green line
+//will not draw anything after drawn
 	ldr r0, =0xa			//x coordinate of knight bee
 	ldr r1, =0x200			//y coordinate of knight bee
 	push {r0, r1}
 	bl drawBeeK
 	pop {r0, r1}
+
 	
-testQueen:
+testQueen:		
+//currently draws a yellow rectangle down of where it should be, and a thick black vertical line
+//will not draw anything after drawn
 	ldr r0, =0x2f5			//x coordinate of queen bee
 	ldr r1, =0x200			//y coordinate of queen bee
 	push {r0, r1}
 	bl drawBeeK
 	pop {r0, r1}
 	
-testBush:
-	ldr r0, =0x180			//x coordinate of bush
-	ldr r1, =0x3f5			//y coordinate of bush
-	ldr r2, #3				//size of bush
+*/
+testBush:		//needs to take the size inputted and multiply it to make a recognizable size
+	ldr r0, =0x20			//x coordinate of bush
+	ldr r1, =0x20			//y coordinate of bush
+	mov r2, #20				//size of bush
 	push {r0, r1, r2}
 	bl drawBush
 	pop {r0, r1, r2}
+
 	
-testPlayer:
+testPlayer:		//draws player slightly left and very down of where it should be
+			//will not draw anything after being drawn
 	ldr r0, =0x180			//x coordinate of player
 	ldr r1, =0x200			//y coordinate of player
 	push {r0, r1}
 	bl drawPlayer
 	pop {r0, r1}
 	
+
 testRect:
 	ldr r0, =0xc0			//x coordinate of rectangle
 	ldr r1, =0x100			//y coordinate of rectangle
 	ldr r2, =0x0			//color of rectangle
-	ldr r3, #5				//length of rectangle
-	ldr r4, #5				//width of rectangle
+	mov r3, #20				//length of rectangle
+	mov r4, #40				//width of rectangle
 	push {r0, r1, r2, r3, r4}
 	bl drawRect
 	pop {r0, r1}
 	
+
 	
