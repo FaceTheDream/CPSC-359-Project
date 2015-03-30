385dec148b9dca77f052c49148aaed5d0709cde7
Itachi0970 <dkennygold@hotmail.com>
2015-03-29 18:21:49 -0600

    Update logic.s

--- logic.s
+++ logic.s
@@ -5,7 +5,7 @@ start:	//start up the game
 	mov r1, #0
 	str r1, [r0]
 	ldr r0, =playerx	//move player to middle of screen
-	ldr r1, =#382
+	ldr r1, =0x17E		//382
 	str r1, [r0]
 	ldr r0, =playery
 	mov r1, #512
@@ -153,7 +153,7 @@ npcStuff:
 	cmp r5, #0			//skip update if hp = 0
 	beq npcStuff
 	//puts random number %4 in r2
-	ldr r3, =#69069
+	ldr r3, =0x10DCD	//69069
 	ldr r2, =x20003004
 	ldr r2, [r2]		//current clock value
 	mul r2, r3, r2
@@ -248,7 +248,7 @@ bullDown:
 	str r2, [r2, r0, lsl #2]
 	ldr r1, =bulletfaces
 	ldr r3, [r1, r0, lsl #2]
-	ldr r4, =#767
+	ldr r4, =0x2FF		//767
 	cmp r2, r4
 	moveq r3, #5
 	str r3, [r1, r0, lsl #2]
@@ -275,7 +275,7 @@ bullRight:
 	str r2, [r2, r0, lsl #2]
 	ldr r1, =bulletfaces
 	ldr r3, [r1, r0, lsl #2]
-	ldr r4, =#1023
+	ldr r4, =0x3FF		//1023
 	cmp r2, r4
 	moveq r3, #5
 	str r3, [r1, r0, lsl #2]
@@ -512,7 +512,7 @@ playerDown:
 	str r1, [r0]
 	ldr r0, =playery
 	ldr r1, [r0]
-	ldr r4, =#767
+	ldr r4, =0x2FF		//767
 	cmp r1, r4
 	beq endSub
 	add r1, r1, #1
@@ -537,7 +537,7 @@ playerRight:
 	str r1, [r0]
 	ldr r0, =playerx
 	ldr r1, [r0]
-	ldr r4, =#1023
+	ldr r4, =0x3FF		//1023
 	cmp r1, r4
 	beq endSub
 	add r1, r1, #1
@@ -588,7 +588,7 @@ npcDown:
 	ldr r0, [r0]
 	ldr r1, =npcys
 	ldr r2, [r1, r0, lsl #2]
-	ldr r4, =#767
+	ldr r4, =0x2FF		//767
 	cmp r2, r4
 	beq endSub
 	add r2, r2, #1
@@ -617,7 +617,7 @@ npcRight:
 	ldr r0, [r0]
 	ldr r1, =npcxs
 	ldr r2, [r1, r0, lsl #2]
-	ldr r4, =#1023
+	ldr r4, =0x3FF		//1023
 	cmp r2, r4
 	beq endSub
 	sub r2, r2, #1
